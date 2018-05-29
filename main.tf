4420a2457e04de62771e6d4a8c860cbf15cd281f
Helder Klemp <helder.klemp@cmdsolutions.com.au>
2018-05-29 13:38:43 +1000

    adding support to AWS Assume role

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
-module "s3" {
-  source = "modules/s3"
-  branch = "feature"
+provider "aws" {
+  region = "${var.aws_region}"
+
+  assume_role {
+    role_arn     = "${var.role_arn}"
+    session_name = "${var.session_name}"
+    external_id  = "${var.external_id}"
+  }
+}
+
+module "ecs" {
+  source           = "./modules/ecs"
+  ecs_cluster_name = "helderklemp-first-cluster"
 }
--- variabes.tf
+++ variabes.tf
@@ -6,3 +6,15 @@ variable "aws_region" {
   description = "EC2 Region for the VPC"
   default     = "ap-southeast-2"
 }
+
+variable "external_id" {
+  description = "external_id"
+}
+
+variable "role_arn" {
+  description = "role_arn"
+}
+
+variable "session_name" {
+  description = "terraform_clien session namet"
+}
